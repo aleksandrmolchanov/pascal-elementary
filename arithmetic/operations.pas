3ef8ee37f3ba343fb8579b6ed2bb800018c94c22
Aleksandr Molchanov <alexandr87m@gmail.com>
2023-04-01 18:37:14 +0500

    Add arithmetic operations examples

---
+++ arithmetic/operations.pas
@@ -0,0 +1,10 @@
+program operations;
+begin
+	writeln('22+35 = ', 22+35);
+	writeln('126-24 = ', 126-24);
+	writeln('4*3 = ', 4*3);
+	writeln('14/7 = ', 14/7);
+	writeln('14/7:7:3 = ', 14/7:7:3);
+	writeln('27 div 4 = ', 27 div 4);
+	writeln('27 mod 4 = ', 27 mod 4)
+end.
