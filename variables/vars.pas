026a8fca28ceec2e333dfd0568670add191c6657
Aleksandr Molchanov <alexandr87m@gmail.com>
2023-04-01 19:09:53 +0500

    Add variables initialization examples

---
+++ variables/vars.pas
@@ -0,0 +1,23 @@
+program variables;
+var
+	x: integer;
+	y: integer;
+	flag: boolean;
+	l, m, n: integer;
+	k: integer = 200;
+begin
+	x := 98;
+	y := 200;
+	flag := true;
+	l := 4;
+	m := 5;
+	n := 6;
+
+	writeln('x: ', x);
+	writeln('y: ', y);
+	writeln('flag: ', flag);
+	writeln('l: ', l);
+	writeln('m: ', m);
+	writeln('n: ', n);
+	writeln('k: ', k)
+end.
